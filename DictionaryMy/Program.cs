03d07c817d6afeb804d9ea9d3365475d790dc642
Batuhan <Batuhan@DESKTOP-9BJLHJ4>
2021-01-23 17:38:50 +0300

    Gün 4 Ödev 5

---
+++ DictionaryMy/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DictionaryMy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> myDict = new MyDictionary<string, string>();
+
+            myDict.Add("B", "55");
+            myDict.Add("O", "33");
+            myDict.Add("F", "43");
+            myDict.Add("S", "23");
+
+            for (int i = 0; i < myDict.keys.Length; i++)
+            {
+                Console.WriteLine(myDict.keys[i] + "--" + myDict.values[i]);
+            }
+        }
+
+
+    }
+}
